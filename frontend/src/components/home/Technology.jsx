c9701d7f6e6fd435fb94c947fecfccae342424dc
Muhammad-Zunain <muhammadzunain2004@gmail.com>
2024-08-27 17:39:59 +0500

    Add more css in technology section

--- frontend/src/components/home/Technology.jsx
+++ frontend/src/components/home/Technology.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import react from '../../assets/react-logo.png';
+import python from '../../assets/python-logo.png';
+import mongo from '../../assets/mongo-logo.png';
+import sql from '../../assets/sql-logo.png';
+import flutter from '../../assets/flutter-logo.png';
+import node from '../../assets/node-logo.png';
+import tailwind from '../../assets/tailwind-logo.png';
+import wordpress from '../../assets/wordpress-logo.png';
+import django from '../../assets/django-logo.png';
 import '../css/Technology.css';
 
 function Technology() {
+    const Logo = [react, python ,mongo, sql, flutter, node, tailwind, wordpress, django]
     return (
         <>
-            <div className="about-header">
-                        <h2>Technologies We <span>Work</span> With </h2>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, ducimus et optio sit iusto voluptates?</p>
-                    </div> 
-            <div className="marquee__about__tech">
-                
-                <div className="marquee">
-                    <div className="marquee__inner">
-                        {[...Array(10)].map((_, index) => (
-                            <React.Fragment key={index}>
-                                <div className="marquee__item">
-                                    <img className="tech-logo" src={react} alt="React Logo" />
-                                </div>
-                                <div className="marquee__line"></div> {/* Line centered between logos */}
-                            </React.Fragment>
-                        ))}
-                        {/* Duplicate the items to create a seamless scroll */}
-                        {[...Array(10)].map((_, index) => (
-                            <React.Fragment key={`dup-${index}`}>
-                                <div className="marquee__item">
-                                    <img className="tech-logo" src={react} alt="React Logo" />
-                                </div>
-                                <div className="marquee__line"></div>
-                            </React.Fragment>
-                        ))}
+            <div className='tech-wrapper'>
+                <div className="tech-header">
+                    <h2><span>Technologies</span> We Work With </h2>
+                    
+                </div>
+                <div className="marquee__about__tech">
+
+                    <div className="marquee">
+                        <div className="marquee__inner">
+                            {Logo.map((item, index) => (
+                                <React.Fragment key={index}>
+                                    <div className="marquee__item">
+                                        <img className="tech-logo" src={item} alt="Logo" />
+                                    </div>
+                                    <div className="marquee__line"></div>
+                                </React.Fragment>
+                            ))}
+                            {Logo.map((val, index) => (
+                                <React.Fragment key={`dup-${index}`}>
+                                    <div className="marquee__item">
+                                        <img className="tech-logo" src={val} alt="Logo" />
+                                    </div>
+                                    <div className="marquee__line"></div>
+                                </React.Fragment>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
